50648b6965f57e913cc7a61d9eb78424c9855d7c
Drew Gonzales <drewgonzales360@gmail.com>
2016-08-27 21:25:31 -0700

    Changes to Route 2

--- app/jsmaps/Lagunta.js
+++ app/jsmaps/Lagunta.js
@@ -14,6 +14,8 @@ var Map = require('../map');
 var Lagunta = Map.generateMap(15,22);
 
 // Big block in the bottom left.
+Map.insertObstruction(Lagunta, 3,3,1,1);
+Map.insertObstruction(Lagunta, 14,21,1,1);
 
 Map.insertGate(Lagunta, "Route2", "north", 5);
 Map.insertGate(Lagunta, "Route7", "east", 18);
--- app/jsmaps/Route2.js
+++ app/jsmaps/Route2.js
@@ -12,15 +12,19 @@ Last Edited: 8/25/16
 ****************************************************************/
 var Map = require('../map');
 
-var Route2 = Map.generateMap(27,30);
+var Route2 = Map.generateMap( 100,80);
 
 // Big block in the bottom left.
-Map.insertObstruction(Route2, 0, 4, 20,26);
-Map.insertObstruction(Route2, 1 ,1,1,1);
-Map.insertObstruction(Route2, 4 ,3,4,1);
-Map.insertObstruction(Route2, 11,0,3,1);
-Map.insertObstruction(Route2, 19,4,4,1);
+Map.insertObstruction(Route2, 0, 10, 90, 70);
+Map.insertObstruction(Route2, 0,  12, 13,  1);
+Map.insertObstruction(Route2, 0,  30, 12,  1);
 
+Map.insertObstruction(Route2, 40, 4,  5, 1);
+Map.insertObstruction(Route2, 95, 15, 1, 15);
+Map.insertObstruction(Route2, 91, 50, 1, 20);
+Map.insertObstruction(Route2, 99, 60, 2, 9);
+
+Map.insertObstruction(Route2, 99,99,1,1);
 Map.insertGate(Route2, "SproulTown", "west", 0);
 Map.insertGate(Route2, "Lagunta", "south", 25);
 Map.initMap(Route2, "Route2", 0,65);
